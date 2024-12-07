52a2b921364605449aa2b24269105e292f1f6375
Abraham Vijai <abrahamvijai03@gmail.com>
2024-12-06 20:07:53 -0500

    Added filter functionality

---
+++ filter.js
@@ -0,0 +1,23 @@
+class Filter {
+    constructor() {
+        
+    }
+
+    static invert() {
+        appliedFilter = "invert";
+    }
+
+    static posterize() {
+        appliedFilter = "posterize";
+    }
+
+    static gray() {
+        appliedFilter = "gray";
+    }
+
+    static blur() {
+        appliedFilter = "blur";
+    }
+
+
+}
--- sketch.js
+++ sketch.js
@@ -1,7 +1,30 @@
+let video; 
+const VIDEO_WIDTH = 640
+const VIDEO_HEIGHT = 480
+let appliedFilter = "opaque"
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight); // Create a canvas of size 640x480
+  
+  // Create a video capture object
+  video = createCapture(VIDEO);
+  video.size(VIDEO_WIDTH, VIDEO_HEIGHT); 
+  video.hide(); 
+
+  UI.setupUI();
 }
 
 function draw() {
-  background(220);
+  background(150); 
+
+  // Draw the captured video feed centered horizontally and vertically
+  image(video, (width - video.width) / 2, (height - video.height) / 2);
+  filter(appliedFilter)
+
+  if(UI.HasFilter.checked()){
+    appliedFilter = "opaque"
+  }
 }
+
+
+
+
